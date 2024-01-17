a2b678ce0cfb459565c053a89d3d3749576dadf5
isobel-bosman <isobel.bosman@gmail.com>
2024-01-17 17:31:32 +0200

    Added tailwind

---
+++ app/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['.index.html', './src/**/*.{html,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: 'var(--primary)',
+          100: 'var(--primary-100)',
+          200: 'var(--primary-200)',
+          300: 'var(--primary-300)',
+          400: 'var(--primary-400)',
+          600: 'var(--primary-600)',
+          700: 'var(--primary-700)',
+          800: 'var(--primary-800)',
+          900: 'var(--primary-900)',
+        },
+        secondary: {
+          DEFAULT: 'var(--secondary)',
+        },
+        accent: {
+          DEFAULT: 'var(--accent)',
+        },
+        warning: 'var(--warning)',
+        error: 'var(--error)',
+        success: 'var(--success)',
+      },
+    },
+  },
+  plugins: [],
+};
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import EnvironmentPlugin from 'vite-plugin-environment';
+import vitePluginCommonJS from 'vite-plugin-commonjs';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import dns from 'dns';
+
+dns.setDefaultResultOrder('verbatim');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '',
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    // eslint(),
+    EnvironmentPlugin('all'),
+    vitePluginCommonJS(),
+  ],
+  server: {
+    // this ensures that the browser opens upon server start
+    open: false,
+    host: 'localhost',
+    // this sets a default port to 3000
+    port: 3000,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
+});
