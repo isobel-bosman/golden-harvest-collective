8b6f5d64093c8f86c5482989652c71263afaf348
isobel-bosman <isobel.bosman@gmail.com>
2024-01-24 16:10:25 +0200

    Added a favicon to the top and aligned the title in the header

--- app/src/components/header/header.tsx
+++ app/src/components/header/header.tsx
@@ -3,14 +3,11 @@ import React from 'react';
 const Header = () => {
   return (
     <div className="fixed top-0 z-10 h-16 w-full bg-secondary shadow">
-      <div className="flex h-full w-full items-center justify-between px-5 sm:px-10">
-        <a className="text-lg font-semibold text-accent" href="/">
+      <div className="flex h-full w-full items-center justify-between px-5 sm:px-20">
+        <a className="text-xl font-semibold text-accent" href="/">
           Golden Harvest Collective
         </a>
-        <span
-          className="hidden items-center gap-5 sm:flex
-        "
-        >
+        <span className="hidden items-center gap-5 sm:flex">
           <a className="text-base text-accent" href="/">
             Home
           </a>
