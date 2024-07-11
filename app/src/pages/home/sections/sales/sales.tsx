2ed33be79a817675c64995b510f5924b7cb73f64
isobel-bosman <isobel.bosman@gmail.com>
2024-07-11 08:23:53 +0200

    style: :lipstick: Updated the styling of the sales cards to accomodate smaller screens

--- app/src/pages/home/sections/sales/sales.tsx
+++ app/src/pages/home/sections/sales/sales.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import QRCode from '../../../../assets/images/qr-code.png';
+import MultiCardCarousel from '../../../../components/multi-card-carousel/multi-card-carousel';
 
 const Sales = () => {
   return (
@@ -11,26 +12,130 @@ const Sales = () => {
         Sales
       </h2>
 
-      <div className="flex flex-col sm:flex-row w-full h-full gap-5 sm:gap-20">
-        <div className="rounded-xl bg-white w-full py-5 px-16">
+      <div className="flex flex-col md:flex-row w-full h-full gap-5 lg:gap-10 xl:gap-20">
+        <div className="rounded-xl bg-white w-full py-5 px-5">
+          <h3 className="text-secondary text-xl font-medium text-center">
+            Price List
+          </h3>
+          <div className="flex flex-wrap ">
+            <div className="flex basis-full justify-between items-center">
+              <h4 className="text-secondary w-40 text-lg font-medium ">
+                Raw Honey
+              </h4>
+              <hr className="w-full border-primary" />
+            </div>
+            <div className="flex basis-full justify-between">
+              <p className="text-secondary ">375g (squeeze) </p>
+              <p className="text-secondary ">R60 </p>
+            </div>
+            <div className="flex basis-full justify-between">
+              <p className="text-secondary ">500g (squeeze) </p>
+              <p className="text-secondary ">R80 </p>
+            </div>
+            <div className="flex basis-full justify-between">
+              <p className="text-secondary ">1Kg (squeeze) </p>
+              <p className="text-secondary ">R140 </p>
+            </div>
+            <div className="flex basis-full justify-between">
+              <p className="text-secondary ">1.5Kg (tub) </p>
+              <p className="text-secondary ">R200 </p>
+            </div>
+            <div className="flex basis-full justify-between">
+              <p className="text-secondary ">7Kg (tub) </p>
+              <p className="text-secondary ">R785 </p>
+            </div>
+
+            <div className="mt-4 flex basis-full justify-between items-center">
+              <h4 className="text-secondary w-56 text-lg font-medium ">
+                Creamed Honey
+              </h4>
+              <hr className="basis-full border-primary" />
+            </div>
+            <div className="flex basis-full justify-between">
+              <p className="text-secondary ">300g ALOE </p>
+              <p className="text-secondary ">R50 </p>
+            </div>
+
+            <div className="mt-4 flex basis-full justify-between items-center">
+              <h4 className="text-secondary w-44 text-lg font-medium ">
+                Honeycomb
+              </h4>
+              <hr className="basis-full border-primary" />
+            </div>
+            <div className="flex basis-full justify-between">
+              <p className="text-secondary ">350g</p>
+              <p className="text-secondary ">R65 </p>
+            </div>
+
+            <div className="mt-4 flex basis-full justify-between items-center">
+              <h4 className="text-secondary text-lg font-medium w-40">
+                Gift Favours
+              </h4>
+              <hr className="basis-full border-primary" />
+            </div>
+            <div className="flex basis-full justify-between">
+              <p className="text-secondary ">165g</p>
+              <p className="text-secondary ">R43 </p>
+            </div>
+
+            <div className="mt-4 flex basis-full justify-between items-center">
+              <h4 className="text-secondary text-lg font-medium w-60">
+                Food Grade Wax
+              </h4>
+              <hr className="basis-full border-primary" />
+            </div>
+            <div className="flex basis-full justify-between">
+              <p className="text-secondary ">per Kg</p>
+              <p className="text-secondary ">R280 </p>
+            </div>
+
+            <div className="mt-4 flex basis-full justify-between items-center">
+              <h4 className="text-secondary text-lg font-medium w-40">
+                Bulk Orders
+              </h4>
+              <hr className="basis-full border-primary" />
+            </div>
+            <div className="flex basis-full justify-between">
+              <p className="text-secondary ">375g x 24 (box)</p>
+              <p className="text-secondary ">R1300</p>
+            </div>
+            <div className="flex basis-full justify-between">
+              <p className="text-secondary ">500g x 24 (box)</p>
+              <p className="text-secondary ">R1725</p>
+            </div>
+            <div className="flex basis-full justify-between">
+              <p className="text-secondary ">1Kg x 12 (box)</p>
+              <p className="text-secondary ">R1620</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="rounded-xl bg-white w-full py-5 px-5">
           <h3 className="text-secondary text-xl font-medium text-center ">
             Buy Now
           </h3>
           <p className="my-5 text-center">
-            Scan this code to view our Whatsapp catalouge
+            Scan this code to browse our Whatsapp catalouge (or simply click on
+            it)
           </p>
-          <div className="flex items-center sm:p-10">
-            <img className="w-full" src={QRCode}></img>
+          <div className="flex items-center sm:p-10 p-5">
+            <img
+              className="w-full min-w-40 cursor-pointer"
+              src={QRCode}
+              onClick={() =>
+                window.open('https://wa.me/c/27815258959', '_blank')
+              }
+            ></img>
           </div>
         </div>
-        <div className="rounded-xl bg-white w-full py-5 sm:px-16 px-5">
+        <div className="rounded-xl bg-white w-full py-5 px-5 flex flex-col justify-between">
           <h3 className="text-secondary text-xl font-medium text-center">
             Bulk Sales
           </h3>
 
           <div className="flex flex-wrap">
-            <div className="mt-4 flex basis-full justify-between items-center">
-              <h4 className="text-secondary text-lg font-medium w-64">
+            <div className="flex basis-full justify-between items-center">
+              <h4 className="text-secondary text-lg font-medium max-w-64">
                 Wildflower and Cosmos
               </h4>
               <hr className="basis-full border-primary" />
@@ -48,8 +153,8 @@ const Sales = () => {
               <p className="text-secondary font-medium">R75.00 pkg</p>
             </div>
 
-            <div className="mt-4 flex basis-full justify-between items-center">
-              <h4 className="text-secondary text-lg font-medium w-96">
+            <div className="mt-6 flex basis-full justify-between items-center">
+              <h4 className="text-secondary text-lg font-medium max-w-96">
                 Seasonal Premium Varieties
               </h4>
               <hr className="w-full border-primary" />
@@ -67,11 +172,10 @@ const Sales = () => {
               <p className="text-secondary font-medium">R75.00 pkg</p>
             </div>
           </div>
-        </div>
-        <div className="rounded-xl bg-white w-full py-5 px-16">
-          <h3 className="text-secondary text-xl font-medium text-center">
-            Become a Distributer
-          </h3>
+          <p className="mt-6 text-secondary ">
+            Please contact Shane Boyd at shane@goldenharvestco.co.za for
+            enquiries
+          </p>
         </div>
       </div>
     </div>
--- app/src/pages/home/sections/seasonal-varieties/seasonal-varieties.tsx
+++ app/src/pages/home/sections/seasonal-varieties/seasonal-varieties.tsx
@@ -6,18 +6,23 @@ const SeasonalVarieties = () => {
   return (
     <div id="seasonal-varieties" className="w-full bg-secondary p-3 sm:p-10">
       <div className="flex w-full flex-wrap h-full justify-center items-center rounded border border-solid border-white bg-secondary p-5">
-        <h2 className="mb-8 basis-full text-center text-3xl font-medium text-primary">
-          Seasonal Variaties
+        <h2 className="basis-full text-center text-3xl font-medium text-primary">
+          Seasonal variaties
         </h2>
+        <p className="mb-8 text-primary basis-full text-center">
+          Please note that the times listed below are for
+          <b> Harvesting Times </b>
+          and the honey varieties are subject to availability.
+        </p>
         <HexagonGrid className="w-full overflow-hidden sm:w-3/4 md:w-1/3">
-          <Hexagon title="Wild Flower" content="All season" icon="wildFlower" />
           <Hexagon title="Blue Gum" content="Dec - Apr" icon="blueGum" />
           <Hexagon
             title="Buffulo Thorn"
             content="Jan - Feb"
             icon="buffuloThorn"
           />
-          <Hexagon title="Cosmos Flower" content="Apr - Jul" icon="cosmos" />
+          <Hexagon title="Cosmos" content="Apr - Jul" icon="cosmos" />
+          <Hexagon title="Wildflower" content="All season" icon="wildFlower" />
           <Hexagon title="Macadamia" content="Sep - Nov" icon="macadamia" />
           <Hexagon title="Citrus" content="Jul - Sep" icon="citrus" />
           <Hexagon
@@ -26,109 +31,6 @@ const SeasonalVarieties = () => {
             icon="eucalyptus"
           />
         </HexagonGrid>
-        <div className="rounded-xl bg-white w-full mb-4 p-10 sm:w-3/4 md:w-1/3 md:mx-20">
-          <div className="h-full flex flex-col items-center">
-            <h3 className="text-secondary text-xl font-medium">Price List</h3>
-            <div className="flex flex-wrap ">
-              <div className="flex basis-full justify-between items-center">
-                <h4 className="text-secondary w-40 text-lg font-medium ">
-                  Raw Honey
-                </h4>
-                <hr className="w-full border-primary" />
-              </div>
-              <div className="flex basis-full justify-between">
-                <p className="text-secondary ">375g (squeeze) </p>
-                <p className="text-secondary ">R60 </p>
-              </div>
-              <div className="flex basis-full justify-between">
-                <p className="text-secondary ">500g (squeeze) </p>
-                <p className="text-secondary ">R80 </p>
-              </div>
-              <div className="flex basis-full justify-between">
-                <p className="text-secondary ">1Kg (squeeze) </p>
-                <p className="text-secondary ">R140 </p>
-              </div>
-              <div className="flex basis-full justify-between">
-                <p className="text-secondary ">1.5Kg (tub) </p>
-                <p className="text-secondary ">R200 </p>
-              </div>
-              <div className="flex basis-full justify-between">
-                <p className="text-secondary ">7Kg (tub) </p>
-                <p className="text-secondary ">R785 </p>
-              </div>
-
-              <div className="mt-4 flex basis-full justify-between items-center">
-                <h4 className="text-secondary w-56 text-lg font-medium ">
-                  Creamed Honey
-                </h4>
-                <hr className="basis-full border-primary" />
-              </div>
-              <div className="flex basis-full justify-between">
-                <p className="text-secondary ">300g ALOE </p>
-                <p className="text-secondary ">R50 </p>
-              </div>
-
-              <div className="mt-4 flex basis-full justify-between items-center">
-                <h4 className="text-secondary w-44 text-lg font-medium ">
-                  Honeycomb
-                </h4>
-                <hr className="basis-full border-primary" />
-              </div>
-              <div className="flex basis-full justify-between">
-                <p className="text-secondary ">350g</p>
-                <p className="text-secondary ">R65 </p>
-              </div>
-
-              <div className="mt-4 flex basis-full justify-between items-center">
-                <h4 className="text-secondary text-lg font-medium w-40">
-                  Gift Favours
-                </h4>
-                <hr className="basis-full border-primary" />
-              </div>
-              <div className="flex basis-full justify-between">
-                <p className="text-secondary ">165g</p>
-                <p className="text-secondary ">R43 </p>
-              </div>
-
-              <div className="mt-4 flex basis-full justify-between items-center">
-                <h4 className="text-secondary text-lg font-medium w-60">
-                  Food Grade Wax
-                </h4>
-                <hr className="basis-full border-primary" />
-              </div>
-              <div className="flex basis-full justify-between">
-                <p className="text-secondary ">per Kg</p>
-                <p className="text-secondary ">R280 </p>
-              </div>
-
-              <div className="mt-4 flex basis-full justify-between items-center">
-                <h4 className="text-secondary text-lg font-medium w-40">
-                  Bulk Orders
-                </h4>
-                <hr className="basis-full border-primary" />
-              </div>
-              <div className="flex basis-full justify-between">
-                <p className="text-secondary ">375g x 24 (box)</p>
-                <p className="text-secondary ">R1300</p>
-              </div>
-              <div className="flex basis-full justify-between">
-                <p className="text-secondary ">500g x 24 (box)</p>
-                <p className="text-secondary ">R1725</p>
-              </div>
-              <div className="flex basis-full justify-between">
-                <p className="text-secondary ">1Kg x 12 (box)</p>
-                <p className="text-secondary ">R1620</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* <HexagonGrid >
-              <Hexagon />
-              <Hexagon />
-              <Hexagon />
-              <Hexagon /> 
-           </HexagonGrid> */}
       </div>
     </div>
   );
