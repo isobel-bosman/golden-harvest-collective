4847b6bb58f2f3bbb2087ae42c14a7a941abdaa4
isobel-bosman <isobel.bosman@gmail.com>
2024-07-11 08:22:37 +0200

    feat: :sparkles: Added product page

--- app/src/pages/home/home.page.tsx
+++ app/src/pages/home/home.page.tsx
@@ -6,20 +6,29 @@ import AboutUs from './sections/about-us/about-us';
 import SeasonalVarieties from './sections/seasonal-varieties/seasonal-varieties';
 import Sales from './sections/sales/sales';
 import ContactUs from './sections/contact-us/contact-us';
+import Products from './sections/products/products';
 
 const Home = () => {
   return (
     <div className="flex w-full flex-col overflow-x-hidden">
       <LandingSection />
+
       <RevealOnScroll>
         <AboutUs />
       </RevealOnScroll>
+
+      <RevealOnScroll>
+        <Products />
+      </RevealOnScroll>
+
       <RevealOnScroll>
         <SeasonalVarieties />
       </RevealOnScroll>
+
       <RevealOnScroll>
         <Sales />
       </RevealOnScroll>
+
       <RevealOnScroll>
         <ContactUs />
       </RevealOnScroll>
